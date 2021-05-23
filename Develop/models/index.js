d6ba51ac066dfa7986e8d39be16d918e0c2fae81
Kate McColl <kate@katemccollart.com>
2021-05-23 14:12:46 +1000

    add: association methods

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,14 +6,33 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey
-})
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tags'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_products'
+})
 
 module.exports = {
   Product,
