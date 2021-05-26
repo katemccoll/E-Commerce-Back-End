290649fa873224a8a2a0c123339452b4e9c7ccbe
Kate McColl <kate@katemccollart.com>
2021-05-26 10:56:32 +1000

    add: update and delete; fix: issues with get by id

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product, ProductTag, Category } = require('../../models');
 
 // The `/api/tags` endpoint
 
@@ -17,12 +17,12 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-    try{
+    try {
         const tagData = await Tag.findByPk(req.params.id, {
-        include: [{ model: Product }]
+            include: [{ model: Product, through: ProductTag, as: "tag_products" }],
         });
         if (!tagData) {
-            res.status(404).json({message: 'No tags found with this id'})
+            res.status(404).json({ message: 'No products found with that id!'});
             return;
         }
         res.status(200).json(tagData);
@@ -41,12 +41,40 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+   try {
+       const tagData = await Tag.update(req.body, {
+           where: {
+               id: req.params.id,
+           },
+       });
+       if (!tagData[0]) {
+           res.status(404).json({ message: 'No tag with this id!' });
+           return;
+       }
+       res.status(200).json(tagData);
+   } catch (err) {
+       res.status(500).json(err);
+   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+    try {
+        const tagData = await Tag.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!tagData) {
+            res.status(404).json({ message: 'No tag found with that id!' });
+            return;
+        }
+        res.status(200).json(tagData);
+        } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
